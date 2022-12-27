d0311b11f51cde6cc58772ef1cdfd590f1ca6d3a
Sergey <gambitttt@mail.ru>
2022-12-27 16:09:00 +0300

    удалил черновики

--- Program.cs
+++ Program.cs
@@ -1,20 +1,4 @@
-﻿/*Console.WriteLine("Введите число ");
-int n = int.Parse(Console.ReadLine()!);
-for (int i = 1; i <= n; i++){
-    Console.Write($"{Math.Pow(i, 2)} ");
-}*/
-
-/*Console.WriteLine("Введите координату Х точки1: ");
-int xa = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите координату Y точки1: ");
-int ya = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите координату Х точки2: ");
-int xb = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите координату Y точки2: ");
-int yb = int.Parse(Console.ReadLine()!);
-double res = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
-Console.WriteLine($"Расстояние между точками: {res:f2}");*/
-
+﻿
 /*Задача 1
 Console.WriteLine("Введите любое 5-ти значное число");
 int number = int.Parse(Console.ReadLine()!);
